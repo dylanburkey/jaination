8d129bb5fa32fad0ca3fd312fb5f87e463fac01f
Dylan Burkey <dylan_burkey@lincolnelectric.com>
2016-05-09 22:54:26 -0400

    wp-config
    Update wp-config

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'dbcarl');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'dylanaburkey');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'viking101');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.carl.southernrealm-work.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
