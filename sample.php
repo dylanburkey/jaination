7b13c809ddbef990ec23f0814b2c70880ea12333
Dylan Burkey <dylan_burkey@lincolnelectric.com>
2016-05-09 20:29:03 -0400

    Initial
    Init Commit

---
+++ sample.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="container">
+			<header role="banner">
+				
+
+			</header>
+			<main role="main">
+				
+
+			</main>
+
+			<footer role="contentinfo">
+				
+
+			</footer>
+	</div>
+</body>
+</html>
